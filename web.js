6335a61c5869a5feb31e1a048b76846bbfe82152
Thaissa <thaissa.falbo@resultadosdigitais.com.br>
2019-07-25 14:39:54 -0300

    fixes deploy

--- web.js
+++ web.js
@@ -1,12 +1,10 @@
-var express = require("express");
-var logfmt = require("logfmt");
+var express = require('express');
+var morgan = require('morgan');
 var app = express();
+var port = process.env.PORT || 5000;
 
-app.use(logfmt.requestLogger());
-
-app.use("/", express.static(__dirname));
-
-var port = Number(process.env.PORT || 5000);
+app.use(morgan('combined'));
+app.use('/', express.static(__dirname));
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log('Server started on ' + port);
 });
